567a0d662aabf65a976a513fc5e76a66f6937c5e
Ricardo Serradas <riserrad@microsoft.com>
2017-12-04 17:05:16 +0000

    Added file UpdateBuildNumber.ps1

---
+++ UpdateBuildNumber.ps1
@@ -0,0 +1,20 @@
+## Update Build Number
+
+write-host $env:BUILD_SOURCEBRANCHNAME
+write-host $env:BUILD_SOURCEBRANCH
+
+$version = ""
+
+if ($env:BUILD_SOURCEBRANCH -like "refs/pull*") {
+ $version = "beta"
+ Write-Output ("##vso[build.updatebuildnumber]" + $env:BUILD_BUILDNUMBER+"-$version")
+ Write-Host "Entered here: beta"
+}
+elseif($env:BUILD_SOURCEBRANCHNAME -ne "master"){
+ $version = "alpha"
+ Write-Output ("##vso[build.updatebuildnumber]" + $env:BUILD_BUILDNUMBER+"-$version")
+ Write-Host "Entered here: alpha"
+}
+
+Write-Output ("##vso[task.setvariable variable=UnstableFlag;]$version")
+Write-Host "Set UnstableFlag as: $Env:UnstableFlag"
