608a6448529de9ba9e10c03661e4ff1fb114679a
Ricardo Serradas <riserrad@microsoft.com>
2017-12-12 21:49:52 +0000

    Updated UpdateUnstableFlag.ps1

--- UpdateUnstableFlag.ps1
+++ UpdateUnstableFlag.ps1
@@ -7,14 +7,10 @@ $version = ""
 
 if ($env:BUILD_SOURCEBRANCH -like "refs/pull*"){
  $version = "beta"
- Write-Output ("##vso[task.setvariable variable=UnstableFlag;]$version")
- Write-Host "Entered here: beta"
 }
 elseif($env:BUILD_SOURCEBRANCHNAME -ne "master")
 {
  $version = "alpha"
- Write-Output ("##vso[task.setvariable variable=UnstableFlag;]$version")  
- Write-Host "Entered here: beta"
 }
 
 Write-Output ("##vso[task.setvariable variable=UnstableFlag;]$version")
