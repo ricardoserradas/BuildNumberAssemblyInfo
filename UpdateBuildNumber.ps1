98d4f541bc33fe16f9cd1a2d006ff786b89b8a88
Ricardo Serradas <riserrad@microsoft.com>
2017-12-12 21:49:10 +0000

    Updated UpdateBuildNumber.ps1

--- UpdateBuildNumber.ps1
+++ UpdateBuildNumber.ps1
@@ -7,11 +7,9 @@ $version = ""
 
 if ($env:BUILD_SOURCEBRANCH -like "refs/pull*") {
  $version = "beta"
- Write-Output ("##vso[build.updatebuildnumber]" + $env:BUILD_BUILDNUMBER+"-$version")
- Write-Host "Entered here: beta"
 }
 elseif($env:BUILD_SOURCEBRANCHNAME -ne "master"){
  $version = "alpha"
- Write-Output ("##vso[build.updatebuildnumber]" + $env:BUILD_BUILDNUMBER+"-$version")
- Write-Host "Entered here: alpha"
 }
+
+Write-Output ("##vso[build.updatebuildnumber]" + $env:BUILD_BUILDNUMBER+"-$version")
