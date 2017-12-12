99f5474b9d236705143112762d7eefae5e391f61
Ricardo Serradas <riserrad@microsoft.com>
2017-12-12 21:59:12 +0000

    Updated UpdateUnstableFlag.ps1

--- UpdateUnstableFlag.ps1
+++ UpdateUnstableFlag.ps1
@@ -7,11 +7,14 @@ $version = ""
 
 if ($env:BUILD_SOURCEBRANCH -like "refs/pull*"){
  $version = "beta"
+ Write-Host "Entered here: beta"
 }
 elseif($env:BUILD_SOURCEBRANCHNAME -ne "master")
 {
  $version = "alpha"
+ Write-Host "Entered here: alpha"
 }
 
 Write-Output ("##vso[task.setvariable variable=UnstableFlag;]$version")
-Write-Output "Set UnstableFlag as: $Env:UnstableFlag" -Verbose
+Write-Output "Env variable UnstableFlag: $Env:UnstableFlag" -Verbose
+Write-Output "Local variable UnstableFlag: $version"
